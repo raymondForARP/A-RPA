106556d815bebff312d9931f0672376a3c3fd48b
raymondForARP <Charlie.Raymond@a-rpa.com>
2019-04-15 21:16:28 -0700

    added zip files

--- AutoScript.py
+++ AutoScript.py
@@ -1,14 +1,21 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import pandas as pd
+from time import sleep
+import zipfile
+import os
+import xlrd
+import csv
+from shutil import copyfile
+from datetime import date
 
 
 #This is to specify where we want the files to be downloaded
 options = Options()
 options.add_experimental_option("prefs", {
-  "download.default_directory": r"/Users/charlesraymond/Desktop/Custodian_Folder",
+  "download.default_directory": r"/Users/charlesraymond/A-RPA",
   "download.prompt_for_download": False,
   "download.directory_upgrade": True,
   "safebrowsing.enabled": True
@@ -39,6 +46,63 @@ driver.find_element_by_link_text("OK").click()
 driver.find_element_by_link_text("Select all").click()
 driver.find_element_by_link_text("as xls").click()
 driver.find_element_by_link_text("No").click()
+
+#Sits and waits for zip file.
+#Once downloaded, zip will appear
+zipFiles = []
+
+while not zipFiles:
+    for file in os.listdir("/Users/charlesraymond/A-RPA"):           
+        if file.endswith(".zip"):
+            zipFiles.append(file)
+            print(file)
+
+    sleep(3)
+
+    
+zip_ref = zipfile.ZipFile(zipFiles[0], 'r')
+    
+    
+zip_ref.extractall()
+zip_ref.close() 
+#print(zipFiles[x])
+
+#os.remove(zipFiles[0])
+
+#Gets todays date to document zip file
+today = str(date.today())
+print(today)
+
+date = today + ".zip"
+date = "/Users/charlesraymond/A-RPA/zipFiles/" + date
+copyfile(zipFiles[0], date)
+print("This is the zip file name" + date)
+os.remove(zipFiles[0])
+
+zipFiles.pop()
+xlsFiles = []
+
+for file in os.listdir("/Users/charlesraymond/A-RPA"):
+    if file.endswith(".xls"):
+            #print(type(file))
+        print(file)
+        print("WE GET HERE")
+            #thefile = file.to_string()
+        data_xls = pd.read_excel(file, encoding_override="cp1251" )
+            #data_xls = xlrd.open_workbook(file, logfile=open(os.devnull, 'w'))#encoding_override='cp1252') 
+            
+            #your_csv_file = open('someFile.csv','data_xls')
+            #wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
+        file = file.replace(".xls", ".csv")
+        print("new file name" + file)
+           # for fownum in xrange(sh.nrows):
+               # wr.writerow(sh.row_values(rownum))
+           # your_csv_file.close()
+            #data_xls.to_csv(file, encoding='utf-8', index=False)
+        data_xls.to_csv(file, encoding='utf-8', index=False)
+
+    #print(zipFiles[0])
+#sleep(3)
 
 #print(driver.page_source)
 
